3412cd1a0402cbeb5b08821207e4e2037f96cdaa
Greta Anesko <ganesko3.14@gmail.com>
2025-04-23 11:16:06 -0400

    still won't work

--- data-raw/most_recent_data.R
+++ data-raw/most_recent_data.R
@@ -1,9 +1,8 @@
 ## code to prepare `most_recent_data` dataset goes here
 
-usethis::use_data(most_recent_data, overwrite = TRUE)
-httr::set_config(httr::config(http_version = 1.1))
-usethis::use_zip(
+httr::set_config(httr::config(http_version = 2))
+most_recent_data <- usethis::use_zip(
   "https://www.phmsa.dot.gov/sites/phmsa.dot.gov/files/data_statistics/pipeline/incident_gas_distribution_jan2010_present.zip",
   destdir = getwd()
 )
-
+usethis::use_data(most_recent_data, overwrite = TRUE)
